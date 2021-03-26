65353a45964e90007c4485cebba3b7831fe8ece5
syunta <shunta.kitagawa@toyokumo.co.jp>
2021-03-26 15:13:04 +0900

    Add tests

---
+++ __tests__/vue2/good.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    good
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Good',
+};
+</script>
--- __tests__/vue2/test.js
+++ __tests__/vue2/test.js
@@ -2,11 +2,14 @@ const path = require('path');
 const { lintFilesWithConfigFile } = require('../support/eslint');
 
 const config = path.resolve(__dirname, 'config.js');
-const good = path.resolve(__dirname, 'good.js');
+const goodJs = path.resolve(__dirname, 'good.js');
+const goodVue = path.resolve(__dirname, 'good.vue');
 
 test('vue2', async () => {
-  const lintResult = await lintFilesWithConfigFile(config, [good]);
+  const lintResult = await lintFilesWithConfigFile(config, [goodJs, goodVue]);
 
   expect(lintResult[0].errorCount).toBe(0);
   expect(lintResult[0].warningCount).toBe(0);
+  expect(lintResult[1].errorCount).toBe(0);
+  expect(lintResult[1].warningCount).toBe(0);
 });
