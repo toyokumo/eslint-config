dcbe5f84924e9283b3030c9cd08d9683c85d3f5d
dododoSA <dododosa14@gmail.com>
2021-10-01 15:20:57 +0900

    add: eslint plugins(react react-hooks jsx-a11y)

---
+++ rules/react.js
@@ -0,0 +1,23 @@
+module.exports = {
+  overrides: [{
+    files: ['*.ts', '*.tsx'],
+    plugins: ['react', "jsx-a11y", "react-hooks"],
+    extends: [
+      "plugin:react/recommended",
+      "plugin:react-hooks/recommended",
+      "plugin:jsx-a11y/recommended",
+      "prettier",
+    ],
+    parserOptions: {
+      ecmaFeatures: {
+        "jsx": true,
+      }
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    }
+  }
+  ]
+}
