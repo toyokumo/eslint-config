e968ecde73dbd5d1fdcd5aaf18a237c3ff9f3cb0
dododoSA <dododosa14@gmail.com>
2021-10-01 16:27:04 +0900

    format

--- rules/react.js
+++ rules/react.js
@@ -1,23 +1,24 @@
 module.exports = {
-  overrides: [{
-    files: ['*.ts', '*.tsx'],
-    plugins: ['react', "jsx-a11y", "react-hooks"],
-    extends: [
-      "plugin:react/recommended",
-      "plugin:react-hooks/recommended",
-      "plugin:jsx-a11y/recommended",
-      "prettier",
-    ],
-    parserOptions: {
-      ecmaFeatures: {
-        "jsx": true,
-      }
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      plugins: ['react', 'jsx-a11y', 'react-hooks'],
+      extends: [
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:jsx-a11y/recommended',
+        'prettier',
+      ],
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      settings: {
+        react: {
+          version: 'detect',
+        },
+      },
     },
-    settings: {
-      react: {
-        version: 'detect'
-      }
-    }
-  }
-  ]
-}
+  ],
+};
