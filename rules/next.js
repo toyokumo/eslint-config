94ed0baea7a6a4f7235fe1f904ffa4e3743df5c3
syunta <shunta.kitagawa@toyokumo.co.jp>
2024-04-22 11:37:54 +0900

    format

--- rules/next.js
+++ rules/next.js
@@ -26,13 +26,15 @@ const nextPlugin = require('@next/eslint-plugin-next');
 
  */
 
-module.exports = [{
-  files: ['**/*.ts', '**/*.tsx'],
-  plugins: {
-    '@next/next': nextPlugin,
+module.exports = [
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    plugins: {
+      '@next/next': nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
+    },
   },
-  rules: {
-    ...nextPlugin.configs.recommended.rules,
-    ...nextPlugin.configs['core-web-vitals'].rules,
-  },
-}];
+];
