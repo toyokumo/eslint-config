69e1445c81854ef1a8efdb4bd958bd378ec5f17c
egs33 <egs33.bicycle@gmail.com>
2024-05-15 12:26:39 +0900

    Add prettier config file as devDependencies importable

--- rules/import.js
+++ rules/import.js
@@ -19,6 +19,8 @@ module.exports = {
           '**/webpack.config.js',
           '**/webpack.config.*.js',
           '**/eslint.config.{js,ts}',
+          '**/.prettierrc.{js,cjs,mjs}',
+          '**/prettier.config.{js,cjs,mjs}',
         ],
       },
     ],
